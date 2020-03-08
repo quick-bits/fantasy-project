f1f01acf82f86793980514d7c4163847524af712
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-03-08 15:03:19 +0200

    render1 done

--- Js/content.js
+++ Js/content.js
@@ -1,39 +1,97 @@
 'use strict'
 
-var FormConstructor = function (name ,personality, description  , appears) {
+var darkarry= ['Baba Yaga','Death','Dracula','juker'];
 
-this.name = name;
-this.personality = personality;
-this.description = description;
-this.appears = appears ;
+var pushimg=[];
 
+var FormConstructor = function (name) {
+
+    this.name = name;
+    this.audio=`aud/darkfantasy/${name}.mp3`;
+    this.img=`img/darkfantasy/${name}.png`;
+    pushimg.push(this);
+}
+var ser;
+function creatOpject(){
+    for(var i=0;i<darkarry.length;i++)
+    {
+       ser=  new FormConstructor(darkarry[i]);
+       console.log(pushimg);
+    }
+}
+creatOpject();
+
+
+var contentForm = document.getElementById('contnet-form');
+contentForm.addEventListener('submit', handleSbmit);
+
+function handleSbmit(event) {
+    event.preventDefault();
+    var selectCategory = event.target.category.value;
+
+    var select = document.getElementById('category');
+
+    if (selectCategory === "Dark Fantasy") {
+
+        ser.renderDark();
+    }
+    else if (selectCategory === "Historical Fantasy") {
+
+        renderHistorical();
+    }
+    else if (selectCategory === "Novels Fantasy") {
+
+        renderNovels();
+    }
+    else if (selectCategory === "Anime Fantasy") {
+
+        renderAnime();
+    }
 }
 
 
-var sec1 = document.getElementById('fantasy');
-function renderDark (){
-var header = document.createElement('h1');
-sec1.appendChild(header);
-header.textContent= "bla nak akalnaljnj";
-var par = document.createElement('p');
-sec1.appendChild(par);
-par.textContent ='XSCzvzdvzdvzdvzdcsdcdcdnlsdnlsnvlsnvolsdvsdvdndicjaicaeepajviajipvapivaidnviaviaivadiciad'
-}
 
-var contentForm = document.getElementById ('contnet-form') ;
-contentForm.addEventListener('submit', handleSbmit) ;
-
-function handleSbmit (event) {
-
-var selectCategory = event.target.category.value ;
-
-var select = document.getElementById ('category');
-
-if (  selectCategory=== "Dark Fantasy"  ){ 
-
- renderDark ();
+var sec1 = document.getElementById('dark');
+FormConstructor.prototype.renderDark =function(){
+    for(var i=0; i<4; i++){
+     var image1 = document.createElement('img');
+     image1.setAttribute('src',pushimg[i].img)
+     sec1.appendChild(image1);
+     var namesCharacter = document.createElement('h2');
+     sec1.appendChild(namesCharacter);
+     namesCharacter.textContent= pushimg[i].name;
+    }
+  
 }
 
 
+
+function renderHistorical() {
+    var sec1 = document.getElementById('history');
+    // var header = document.createElement('h1');
+    // sec1.appendChild(header);
+    // header.textContent = "bla nak akalnaljnj";
+    // var par = document.createElement('p');
+    // sec1.appendChild(par);
+    // par.textContent = 'history'
 }
 
+function renderNovels() {
+    var sec1 = document.getElementById('novel');
+    // var header = document.createElement('h1');
+    // sec1.appendChild(header);
+    // header.textContent = "bla nak akalnaljnj";
+    // var par = document.createElement('p');
+    // sec1.appendChild(par);
+    // par.textContent = 'novel'
+}
+
+var sec1 = document.getElementById('renderAnime');
+function renderAnime() {
+    // var header = document.createElement('h1');
+    // sec1.appendChild(header);
+    // header.textContent = "bla nak akalnaljnj";
+    // var par = document.createElement('p');
+    // sec1.appendChild(par);
+    // par.textContent = 'renderAnimedsdgsd'
+}
